7d93a559b4afce36809ff6e7e38c963753a31fbd
Jonathon Saxton <jonathon.a.saxton@gmail.com>
2024-10-14 19:18:19 +1100

    Fixed Face element sets Partially reworked animators (UNFINISHED needs completion) Added in system to take the head generated in UI and apply the same head to the mission NPCs

--- Assets/Scripts/Person/BodyController.cs
+++ Assets/Scripts/Person/BodyController.cs
@@ -3,34 +3,48 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 
-public class BodyAnimController : MonoBehaviour
+using static GameConstants;
+
+public class BodyController : MonoBehaviour
 {
     Transform target;
     Operator parentOperator;
-    bool isPlayer;
-    public Transform headTransform;
+
+    #region Body part variables
+    public GameObject headObject;
+    Transform headTransform;
+    SpriteRenderer headSpriteRenderer;
     public GameObject armsObject;
     Transform armsTransform;
     Animator armsAnimator;
     public GameObject legObject;
     Transform legTransform;
     Animator legAnimator;
+    #endregion
+
+    MissionHeadSet headSet;
     public float rotDeadZone = 0.1f;
 
     // Start is called before the first frame update
     void Start()
     {
+        #region Get components
         parentOperator = GetComponentInParent<Operator>();
+        headTransform = headObject.GetComponent<Transform>();
+        headSpriteRenderer = headObject.GetComponent<SpriteRenderer>();
         armsTransform = armsObject.GetComponent<Transform>();
         armsAnimator = armsObject.GetComponent<Animator>();
         legTransform = legObject.GetComponent<Transform>();
         legAnimator = legObject.GetComponent<Animator>();
+        #endregion
+
         target = parentOperator.rotTarget;
+        StartCoroutine(SpawnHead());
     }
 
     // Update is called once per frame
     void Update()
-    {   
+    {
         RotateToFacePoint(headTransform, target.position);
         RotateToFacePoint(armsTransform, target.position);
         if (parentOperator.motionVec.magnitude > rotDeadZone)
@@ -40,9 +54,9 @@ public class BodyAnimController : MonoBehaviour
         if (parentOperator.motionVec != Vector2.zero)
         {
             legAnimator.SetFloat("motion", parentOperator.motionVec.magnitude / parentOperator.maxSpeed * 2);
-            armsAnimator.SetFloat("ArmsMotion", parentOperator.motionVec.magnitude/parentOperator.maxSpeed * 2);
+            armsAnimator.SetFloat("ArmsMotion", parentOperator.motionVec.magnitude / parentOperator.maxSpeed * 2);
         }
-        armsAnimator.SetBool("Armed",parentOperator.activeWeapon != null);
+        armsAnimator.SetBool("Armed", parentOperator.activeWeapon != null);
     }
 
     public void RotateToFacePoint(Transform t, Vector2 point)//Rotate 't' to face towards 'point'
@@ -52,6 +66,32 @@ public class BodyAnimController : MonoBehaviour
         {
             float angle = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;// Calculate the angle between the two points using the difference and convert it to degrees from radians
             t.rotation = Quaternion.Euler(0, 0, angle);//sets the rotation of the transformation to the new angle.
+        }
+    }
+
+    IEnumerator SpawnHead()
+    {
+        yield return new WaitForEndOfFrame();
+        MissionHeadSet[] g = Resources.LoadAll<MissionHeadSet>("Scriptable Objects/Head Sets");
+        foreach (MissionHeadSet set in g)
+        {
+            Debug.Log(set.name);
+            if (parentOperator.faction.IdCheck(set.setFaction))
+            {
+                for (int index = 0; index < parentOperator.personFace.distinctiveComponent.Length; index++)
+                {
+                    if(parentOperator.personFace.distinctiveComponent[index] == true)
+                    {
+                        Sprite headSprite = set.heads[parentOperator.personFace.faceIndices[index]];
+                        Color headColor = set.colors[parentOperator.personFace.colorIndices[index]];
+                        if (parentOperator.personFace.toColorArr[index])
+                        {
+                            headSpriteRenderer.color = headColor;
+                        }
+                        headSpriteRenderer.sprite = headSprite;
+                    }
+                }
+            }
         }
     }
 }
